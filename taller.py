428bf6d27c7a4dd932b19cd1b461ff70f76a6fb5
kimla <camila115011@gmail.com>
2024-04-15 15:30:44 -0500

    1.100

--- taller.py
+++ taller.py
@@ -1,3 +1,20 @@
-#sfjhbsajadsdd
-a=1
+#Con matplotlib se puede hacer graficos matematicos cientificos y es una biblioteca
 
+import numpy as np
+import matplotlib.pyplot as plt 
+
+#Dibujar una funcion seno
+#Crear un ndarray de una dimensión, 100 elementos equiespacioandos desde o a 2*PI
+
+x= np.linspace(0, 2*np.pi, 100)
+y= np.sin(x)
+
+#Usar matplotlib
+
+plt.figure(figsize=(8,4))
+plt.title("Mi primer grafico cientifico en programación :D")
+plt.xlabel("x")
+plt.ylabel("Seno de x")
+plt.grid(True)
+plt.plot(x,y)
+plt.show()
