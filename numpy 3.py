7f28c5d2791cf2ee9724e522b5725284cf7af32a
kimla <camila115011@gmail.com>
2024-04-15 15:12:31 -0500

    Primer commit :p

--- numpy 3.py
+++ numpy 3.py
@@ -1,1 +1,22 @@
 #taller numpy 
+
+import numpy as np 
+import matplotlib as mp
+
+#Crear um arreglo n dimensional a partir de una lista
+
+a=np.array([[1,5], [7,9]])
+b=np.array([[2,0], [1,3]])
+
+#Sacar el producto punto entre a y b
+
+c= np.dot(a,b)
+print(c)
+
+#Solción de un sistema de ecuaciones expersadas por un matriz com mu,py
+
+matrizSolucion= np.array([5,17])
+
+m= np.linalg.solve(a, matrizSolucion)
+
+print(m)
